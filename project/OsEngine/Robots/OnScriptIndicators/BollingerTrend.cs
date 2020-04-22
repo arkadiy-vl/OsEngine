8d58de2586db9e293dabdbacb41ee1d186ca7543
Arkadiy_vl <arkadiy_vl@inbox.ru>
2020-04-22 21:49:03 +0500

    Исключил из бота BollingerTrend выход по фракталу

--- project/OsEngine/Robots/OnScriptIndicators/BollingerTrend.cs
+++ project/OsEngine/Robots/OnScriptIndicators/BollingerTrend.cs
@@ -17,7 +17,6 @@ namespace OsEngine.Robots.OnScriptIndicators
 
         // индикаторы для робота
         private Aindicator bollinger;
-        private Aindicator fractalN;
 
         // режим работы
         public StrategyParameterString Regime;
@@ -30,9 +29,6 @@ namespace OsEngine.Robots.OnScriptIndicators
 
         // отклонение индикатора Bollinger
         public StrategyParameterDecimal BollingerDeviation;
-
-        // длина индикатора FractalN
-        public StrategyParameterInt FractalNLength;
 
         // объём для входа в позицию
         public StrategyParameterInt VolumePercent;
@@ -54,7 +50,6 @@ namespace OsEngine.Robots.OnScriptIndicators
 
         // последняя цена
         private decimal lastPrice;
-        //MethodOutOfPosition lastMethodOutOfPosition;
 
         // параметры индикаторов
         private decimal upBollinger;
@@ -77,11 +72,10 @@ namespace OsEngine.Robots.OnScriptIndicators
             OnDebug = CreateParameter("Включить отладку", false);
             BollingerLength = CreateParameter("Длина болинжера", 50, 50, 200, 10);
             BollingerDeviation = CreateParameter("Отклонение болинжера", 1.5m, 1.0m, 3.0m, 0.2m);
-            FractalNLength = CreateParameter("Длина фрактала", 21, 5, 100, 5);
             VolumePercent = CreateParameter("Объем входа в позицию (%)", 50, 40, 300, 10);
             Slippage = CreateParameter("Проскальзывание (в шагах цены)", 300, 1, 500, 50);
             VolumeDecimals = CreateParameter("Кол. знаков после запятой для объема", 4, 4, 10, 1);
-            MethodOutOfPosition = CreateParameter("Метод выхода из позиции", "Bollinger", new[] { "Bollinger", "TrailingStop", "FractalStop" });
+            MethodOutOfPosition = CreateParameter("Метод выхода из позиции", "Bollinger", new[] { "Bollinger", "TrailingStop" });
             OnStopForBreakeven = CreateParameter("Вкл. стоп для перевода в безубытк", false);
             MinProfitOnStopBreakeven = CreateParameter("Мин. профит для перевода в безубытк (%)", 10, 5, 20, 1);
 
@@ -92,14 +86,9 @@ namespace OsEngine.Robots.OnScriptIndicators
             bollinger.ParametersDigit[1].Value = BollingerDeviation.ValueDecimal;
             bollinger.Save();
 
-            fractalN = IndicatorsFactory.CreateIndicatorByName("FractalN", name + "FractalN", false);
-            fractalN = (Aindicator)tab.CreateCandleIndicator(fractalN, "Prime");
-            fractalN.ParametersDigit[0].Value = FractalNLength.ValueInt;
-            fractalN.Save();
-
             // подписываемся на события
             tab.CandleFinishedEvent += Tab_CandleFinishedEvent;
-            //tab.PositionClosingFailEvent += Tab_PositionClosingFailEvent;
+            tab.PositionClosingFailEvent += Tab_PositionClosingFailEvent;
             ParametrsChangeByUser += BollingerTrend_ParametrsChangeByUser;
         }
 
@@ -126,12 +115,6 @@ namespace OsEngine.Robots.OnScriptIndicators
                 bollinger.ParametersDigit[1].Value = BollingerDeviation.ValueDecimal;
                 bollinger.Reload();
             }
-
-            if (fractalN.ParametersDigit[0].Value != FractalNLength.ValueInt)
-            {
-                fractalN.ParametersDigit[0].Value = FractalNLength.ValueInt;
-                fractalN.Reload();
-            }
         }
 
         //----------------
@@ -145,11 +128,9 @@ namespace OsEngine.Robots.OnScriptIndicators
             }
 
             int lengthBollinger = (int)bollinger.ParametersDigit[0].Value;
-            int lengthFractal = (int)fractalN.ParametersDigit[0].Value;
 
             if (bollinger.DataSeries[0].Values == null ||
-                candles.Count < lengthBollinger + 2 ||
-                candles.Count < lengthFractal)
+                candles.Count < lengthBollinger + 4)
             {
                 return;
             }
@@ -240,11 +221,6 @@ namespace OsEngine.Robots.OnScriptIndicators
             else if (MethodOutOfPosition.ValueString == "TrailingStop")
             {
                 SetTrailingStop(position);
-            }
-            // выход по стопу, устанавливаемому на предыдущий фрактал
-            else if (MethodOutOfPosition.ValueString == "FractalStop")
-            {
-                SetFractalStop(position);
             }
             else
             {
@@ -378,68 +354,6 @@ namespace OsEngine.Robots.OnScriptIndicators
         }
 
 
-        // Установка стопа на предыдущий фрактал (вариант выхода из позиции по фракталу)
-        private void SetFractalStop(Position position)
-        {
-            // цена активации стоп ордера
-            decimal priceActivation;
-
-            // цена стоп ордера
-            decimal priceOrder;
-
-            // порядковый номер фрактала, по которому будет ставится стоп
-            // нумерация фракталов начинается с конца, последний фрактал - это 1
-            int fractalNumber = 1;
-
-            // если позиция закрывается, то ничего не делаем
-            if (position.State == PositionStateType.Closing || position.CloseActiv == true ||
-               (position.CloseOrders != null && position.CloseOrders.Count > 0))
-            {
-                return;
-            }
-
-            // установка стопа для позиции лонг
-            if (position.Direction == Side.Buy)
-            {
-                // цена активации устанавливается на последний нижний фрактал
-                priceActivation = GetLastFractal(fractalN.DataSeries[1], fractalNumber);
-
-                // если последнего нижнежего фрактала нет,
-                // тогда цена активации устанавливается на последний нижний болинджер
-                if (priceActivation == 0)
-                {
-                    priceActivation = bollinger.DataSeries[1].Last;
-                }
-
-                // цена стоп ордера устанавливается ниже на величину проскальзывания от цены активации
-                priceOrder = priceActivation - Slippage.ValueInt * tab.Securiti.PriceStep;
-
-                tab.CloseAtStop(position, priceActivation, priceOrder);
-                position.SignalTypeClose = SignalTypeClose.FractalStop.ToString();
-            }
-
-            // установка стопа для позиции шорт
-            else if(position.Direction == Side.Sell)
-            {
-                // цена активации устанавливается на последний верхний фрактал
-                priceActivation = GetLastFractal(fractalN.DataSeries[0], fractalNumber);
-
-                // если последнего верхнего фрактала нет,
-                // тогда цена активации устанавливается на последний верхний болинджер
-                if (priceActivation == 0)
-                {
-                    priceActivation = bollinger.DataSeries[0].Last;
-                }
-
-                // цена стоп ордера устанавливается выше на величину проскальзывания от цены активации
-                priceOrder = priceActivation + Slippage.ValueInt * tab.Securiti.PriceStep;
-
-                tab.CloseAtStop(position, priceActivation, priceOrder);
-                position.SignalTypeClose = SignalTypeClose.FractalStop.ToString();
-            }
-        }
-
-
         // Установка стопа для перевода позиции в безубыток при достижении мин. профита
         private void SetStopForBreakeven(Position position)
         {
@@ -504,33 +418,6 @@ namespace OsEngine.Robots.OnScriptIndicators
         }
 
 
-        // получить значение фрактала из индикатора Fractal
-        private decimal GetLastFractal(IndicatorDataSeries series, int fractalNumber)
-        {
-            decimal fractalValue = 0;
-
-            if (series == null ||  fractalNumber < 1)
-            {
-                return 0;
-            }
-
-            while (fractalNumber > 0)
-            {
-                for (int i = series.Values.Count - 1; i >= 0; i--)
-                {
-                    if (series.Values[i] != 0)
-                    {
-                        fractalValue = series.Values[i];
-                        break;
-                    }
-                }
-                fractalNumber--;
-            }
-            return fractalValue;
-
-        }
-
-
         // получить объем входа в позицию по проценту от депозита
         private decimal GetVolumeFromPercentageOfDeposit(decimal price)
         {
@@ -540,7 +427,7 @@ namespace OsEngine.Robots.OnScriptIndicators
             {
                 usdtValue = tab.Portfolio.GetPositionOnBoard().Find(pos => pos.SecurityNameCode == "USDT").ValueCurrent;
             }
-            else if (startProgram.ToString() == "IsTester" || startProgram.ToString() == "IsOsOptimizer")
+            else
             {
                 usdtValue = tab.Portfolio.ValueCurrent;
             }
@@ -559,7 +446,6 @@ namespace OsEngine.Robots.OnScriptIndicators
     {
         Bollinger,
         TrailingStop,
-        FractalStop,
         BreakevenStop
     }
 
